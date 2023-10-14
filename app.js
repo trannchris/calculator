84b7470bf8c9ee3f4e71e99875a0c151c2b9d147
Chris Tran <60266790+trannchris@users.noreply.github.com>
2023-10-14 00:57:32 -0500

    Refactor code for operation display

--- app.js
+++ app.js
@@ -1,68 +1,81 @@
-let firstNum = null,
-  secondNum = null,
-  operator = "";
+let firstNum = "",
+  secondNum = "",
+  currentOperator = null,
+  shouldResetScreen = false;
 
-const display = document.querySelector(".display");
+const lastDisplay = document.querySelector(".display-last");
+const currentDisplay = document.querySelector(".display-current");
 
 const numBtns = document.querySelectorAll(".numBtn");
 Array.from(numBtns).forEach((btn) => {
   btn.addEventListener("click", function () {
-    display.textContent += btn.textContent;
+    if (currentDisplay.textContent === "0" || shouldResetScreen) {
+      resetScreen();
+    }
+    currentDisplay.textContent += btn.textContent;
   });
+});
+
+const decBtn = document.querySelector(".decBtn");
+decBtn.addEventListener("click", function () {
+  if (shouldResetScreen) resetScreen();
+  if (currentDisplay.textContent === "") currentDisplay.textContent = "0";
+  if (currentDisplay.textContent.includes(".")) return;
+  currentDisplay.textContent += ".";
 });
 
 const clearBtn = document.querySelector(".redBtn");
 clearBtn.addEventListener("click", function () {
-  display.textContent = "";
-  firstNum = null;
-  secondNum = null;
-  operator = "";
+  currentDisplay.textContent = "0";
+  lastDisplay.textContent = "";
+  firstNum = "";
+  secondNum = "";
+  currentOperator = null;
 });
 
 const opBtns = document.querySelectorAll(".opBtn");
 Array.from(opBtns).forEach((btn) => {
   btn.addEventListener("click", function () {
-    if (display.textContent == "") {
-      operator = btn.textContent;
-      return;
-    } else {
-      if (firstNum) {
-        if (!operator) {
-          operator = btn.textContent;
-        } else {
-          secondNum = Number(display.textContent);
-          firstNum = operate(operator, firstNum, secondNum);
-        }
-      } else {
-        firstNum = Number(display.textContent);
-      }
-      operator = btn.textContent;
-      secondNum = null;
-      display.textContent = "";
-    }
+    if (currentOperator) evaluate();
+    firstNum = currentDisplay.textContent;
+    currentOperator = btn.textContent;
+    lastDisplay.textContent = `${firstNum} ${currentOperator}`;
+    shouldResetScreen = true;
   });
 });
 
 const equalBtn = document.querySelector(".equal");
-equalBtn.addEventListener("click", function () {
-  if (firstNum && display.textContent && operator) {
-    secondNum = Number(display.textContent);
-    firstNum = operate(operator, firstNum, secondNum);
-    display.textContent = firstNum;
-    secondNum = null;
-    operator = "";
-  }
-});
+equalBtn.addEventListener("click", evaluate);
 
 const deleteBtn = document.querySelector(".blueBtn");
 deleteBtn.addEventListener("click", function () {
-  if (display.textContent) {
-    display.textContent = display.textContent.slice(
-      0,
-      display.textContent.length - 1
-    );
+  if (currentDisplay.textContent) {
+    currentDisplay.textContent = currentDisplay.textContent.slice(0, -1);
   }
 });
+
+function evaluate() {
+  if (!currentOperator || shouldResetScreen) return;
+  if (currentOperator === "÷" && currentDisplay.textContent === "0") {
+    alert("You can't divide by 0!");
+    return;
+  }
+  secondNum = currentDisplay.textContent;
+  currentDisplay.textContent = roundResult(
+    operate(currentOperator, firstNum, secondNum)
+  );
+  lastDisplay.textContent = `${firstNum} ${currentOperator} ${secondNum} =`;
+  currentOperator = null;
+}
+
+function roundResult(number) {
+  return Math.round(number * 1000) / 1000;
+}
+
+function resetScreen() {
+  currentDisplay.textContent = "";
+  shouldResetScreen = false;
+}
 
 function add(a, b) {
   return a + b;
@@ -80,8 +93,10 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate(operator, a, b) {
-  switch (operator) {
+function operate(currentOperator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (currentOperator) {
     case "+":
       return add(a, b);
     case "-":
@@ -89,6 +104,9 @@ function operate(operator, a, b) {
     case "x":
       return multiply(a, b);
     case "÷":
-      return divide(a, b);
+      if (b == 0) return null;
+      else return divide(a, b);
+    default:
+      return null;
   }
 }
